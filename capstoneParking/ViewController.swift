02eaf555a9f6fa494252582959127793b4c87671
Justin Snider <jcsnider100@gmail.com>
2019-03-21 08:11:47 -0600

    Initial Commit

---
+++ capstoneParking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  capstoneParking
+//
+//  Created by Justin Snider on 3/21/19.
+//  Copyright © 2019 Justin Snider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
